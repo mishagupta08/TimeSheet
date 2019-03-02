bbd7bf3833cf339e98138a4966b427eeac6ba291
misha gupta <guptamisha88@gmail.com>
2019-03-02 15:40:07 +0530

    date fix

--- TimeSheet/Scripts/project/timesheet.js
+++ TimeSheet/Scripts/project/timesheet.js
@@ -4,22 +4,33 @@
 
 function SaveTimesheetDetail() {
     $("#error").html("");
-    var loginDetail = $('#timesheetform').serialize();
-      $(".preloader").show();
-    $.ajax({
-        url: '/Timesheet/SaveTimesheetEntry',
-        type: 'Post',
-        datatype: 'Json',
-        data: loginDetail
-    }).done(function (result) {
-        $("#error").html(result);
-        $(".preloader").hide();
-    }).fail(function (error) {
-        $("#loginError").html(error.statusText);
-        $(".preloader").hide();
-        $("#closeError").show();
-        $(".preloader").hide();
-    });
 
+    var datestring = $('#dateinput').val();
+    datearray = datestring.split("-");
+
+    var lengthOfMonth = (datearray[1]).length;
+    var twoDigitMonth = (lengthOfMonth > 1) ? (datearray[1]) : '0' + (datearray[1]);
+    var lengthOfDate  =  ((datearray[0])).length;
+    var twoDigitdate  =  (lengthOfDate > 1) ? (datearray[0]) : '0' + (datearray[0]);
+    var newFromDate   = twoDigitdate + "-" + twoDigitMonth + "-" + datearray[2];
+        
+         $('#dateinput').val(newFromDate);
+         $(".preloader").show();
+         var loginDetail = $('#timesheetform').serialize();
+         $.ajax({
+             url: '/Timesheet/SaveTimesheetEntry',
+             type: 'Post',
+             datatype: 'Json',
+             data: loginDetail
+         }).done(function (result) {
+             $("#error").html(result);
+             $(".preloader").hide();
+         }).fail(function (error) {
+             $("#loginError").html(error.statusText);
+             $(".preloader").hide();
+             $("#closeError").show();
+             $(".preloader").hide();
+         });
+     
     return false;
 }
